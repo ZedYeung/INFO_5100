1f70ff285034a6fe5f48b03055fd2ca9d69eff2e
Yue Yang <shoominga@gmail.com>
2017-10-22 00:04:48 -0700

    Assignment6

--- Assignment6/ATM.java
+++ Assignment6/ATM.java
@@ -78,37 +78,19 @@ public class ATM {
    }
    userData.put()
 
-   public CheckingAccount(int number)
-   {
-      this.number = number;
-   }
-  //方法：存钱
-   public void deposit(double amount)
-   {
+   public void deposit(double amount) {
       balance += amount;
    }
-  //方法：取钱
-   public void withdraw(double amount) throws
-                              InsufficientFundsException
-   {
-      if(amount <= balance)
-      {
+
+   public void withdraw(double amount) {
+      if(amount <= balance) {
          balance -= amount;
-      }
-      else
-      {
-         double needs = amount - balance;
-         throw new InsufficientFundsException(needs);
+      } else {
+         System.out.println("Balance not enough!")
       }
    }
-  //方法：返回余额
-   public double getBalance()
-   {
+
+   public double availableBalance() {
       return balance;
-   }
-  //方法：返回卡号
-   public int getNumber()
-   {
-      return number;
    }
 }
---
+++ Assignment6/User.java
@@ -0,0 +1,15 @@
+class User {
+  String name;
+  int age;
+  String address;
+  String phoneNumber;
+  String bankAccoutNumber;
+
+  public User(String name, int age, String address, String phoneNumber, String bankAccoutNumber) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.bankAccoutNumber = bankAccoutNumber;
+  }
+}
