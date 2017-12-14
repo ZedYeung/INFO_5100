9750a33d6832ef46c367b8c309adf727230a7fc3
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-13 17:31:34 -0800

    score and comments

--- project2/GameDriver.java
+++ project2/GameDriver.java
@@ -1,3 +1,7 @@
+/* Need To improve
+ * Score 15
+ * Don't giveup on something like these. you might come accross developing more complex problems than this.
+ */
 public class GameDriver {
 
 	public static void main(String[] args) {
